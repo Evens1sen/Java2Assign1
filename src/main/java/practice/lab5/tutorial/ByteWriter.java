d4682de62a70f7b99e76ff8b3b58800ddc206d80
mhy <11912919@mail.sustech.edu.cn>
2022-10-12 10:37:23 +0800

    Lab5 tutorial

---
+++ src/main/java/practice/lab5/tutorial/ByteWriter.java
@@ -0,0 +1,29 @@
+package practice.lab5.tutorial;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class ByteWriter {
+
+	public static void main(String[] args) throws URISyntaxException {
+
+		try (FileOutputStream fos = new FileOutputStream("bytewriter_output.txt")){
+
+			String s = "计算机系统";
+			byte[] bytes = s.getBytes();
+
+			fos.write(bytes);
+			fos.flush();//fos.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("The pathname does not exist.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Failed or interrupted when doing the I/O operations");
+			e.printStackTrace();
+		}
+
+	}
+
+}
